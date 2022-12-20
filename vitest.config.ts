ab474aee34b9117e11b776dd8139c0783c16e49a
Aaron Agarunov <agarunov.aaron@gmail.com>
2022-12-20 14:59:08 -0500

    Update scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
