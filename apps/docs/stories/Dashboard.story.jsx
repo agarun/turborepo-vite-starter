38d69e5de3b524f4e2c23d4435a20fbc0cb6362c
Aaron Agarunov <agarunov.aaron@gmail.com>
2022-12-21 18:24:34 -0500

    Add story

---
+++ apps/docs/stories/Dashboard.story.jsx
@@ -0,0 +1,18 @@
+import { Dashboard } from '@myorg/shell';
+import { Box } from '@mui/material';
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: '@myorg／shell/layouts/Dashboard',
+  component: Dashboard,
+  componentSubtitle: 'The Dashboard layout that powers MyOrg portals'
+};
+
+export const DashboardLayout = () => (
+  <Dashboard>
+    <Box p={2}>Welcome!</Box>
+  </Dashboard>
+);
