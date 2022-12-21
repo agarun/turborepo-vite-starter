d6d305100fd7146d2399e097ae59e1106d68b935
Aaron Agarunov <agarunov.aaron@gmail.com>
2022-12-21 18:24:05 -0500

    Add vite config

---
+++ apps/docs/vite.config.js
@@ -0,0 +1,27 @@
+import fs from 'fs/promises';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    loader: 'tsx',
+    include: /\/.*\.[tj]sx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: 'load-js-files-as-jsx',
+          setup(build) {
+            build.onLoad({ filter: /\/.*\.js$/ }, async args => ({
+              loader: 'jsx',
+              contents: await fs.readFile(args.path, 'utf8')
+            }));
+          }
+        }
+      ]
+    }
+  }
+});
