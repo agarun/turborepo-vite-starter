326d46b810bda391d2859ee3512b6547063bf995
Aaron Agarunov <agarunov.aaron@gmail.com>
2022-12-22 12:44:39 -0500

    Add apps

---
+++ packages/shared/src/constants/apps.js
@@ -0,0 +1,28 @@
+import HelpIcon from '@mui/icons-material/Help'; // eslint-disable-line
+// ❗ Please do not modify the `// import HelpIcon...` line above.
+// ❗ It is used by the app generator (`pnpm new-app`) to bootstrap new apps.
+
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+
+const apps = [
+  {
+    href: '/posts',
+    icon: PeopleAltIcon,
+    description: 'MyOrg Posts',
+    color: '#c1d2fb',
+    contactEmail: 'noreply@myorg.com',
+    tags: ['general']
+  },
+  {
+    href: '/login',
+    icon: HelpIcon,
+    description: 'MyOrg Login',
+    color: '#ffffff',
+    contactEmail: 'hi@agarun.com'
+  }
+  // add default apps
+  // ❗ Please do not modify the `// add default apps` line above.
+  // ❗ It is used by the app generator (`pnpm new-app`) to bootstrap new apps.
+];
+
+export { apps };
