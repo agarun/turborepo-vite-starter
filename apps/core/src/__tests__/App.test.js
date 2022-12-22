0442217b210c73f79318f4067438e4e84e2b0a2f
Aaron Agarunov <agarunov.aaron@gmail.com>
2022-12-22 12:54:31 -0500

    Add tests

---
+++ apps/core/src/__tests__/App.test.js
@@ -0,0 +1,13 @@
+import { waitFor } from '@testing-library/react';
+import { renderWithRouter } from '@myorg/shared';
+import App from '../App';
+
+describe('renders without crashing', () => {
+  it('renders homepage', async () => {
+    const { getByRole, getAllByText } = renderWithRouter(<App />);
+
+    await waitFor(() => [getByRole('main')]);
+
+    expect(getAllByText(/dashboard/i)).not.toBeNull();
+  });
+});
