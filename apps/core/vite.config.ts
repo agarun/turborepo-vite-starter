8bb16fb84690b2f753718a70f296c41870f622cd
Aaron Agarunov <agarunov.aaron@gmail.com>
2022-12-22 12:54:12 -0500

    Add public files

---
+++ apps/core/vite.config.ts
@@ -0,0 +1,69 @@
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import type { ViteSentryPluginOptions } from 'vite-plugin-sentry';
+import viteSentry from 'vite-plugin-sentry';
+import { visualizer } from 'rollup-plugin-visualizer';
+// Why relative imports? -> https://github.com/vitejs/vite/issues/5370
+import { services } from '../../packages/shared/src/constants/services';
+import { defaultLibraryViteConfig } from '../../packages/shared/src/utils/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  ...defaultLibraryViteConfig,
+  plugins: [
+    react(),
+    splitVendorChunkPlugin(),
+    ...(process.env.CI && process.env.SENTRY // eslint-disable-line turbo/no-undeclared-env-vars
+      ? [
+          viteSentry({
+            authToken: process.env.REACT_APP_SENTRY_TOKEN,
+            url: process.env.REACT_APP_SENTRY,
+            org: 'msk',
+            project: 'myorg-client',
+            release: '1.0',
+            deploy: {
+              env: 'production'
+            },
+            setCommits: {
+              auto: true
+            },
+            sourceMaps: {
+              include: ['.'],
+              ignore: ['node_modules']
+            }
+          } as ViteSentryPluginOptions)
+        ]
+      : [visualizer()])
+  ],
+  envPrefix: 'REACT_APP_',
+  server: {
+    // https://vitejs.dev/config/server-options.html
+    proxy: {
+      '/auth': process.env.REACT_APP_AUTH_API || services.developmentAuth
+    }
+  },
+  define: {
+    'process.env': Object.keys(process.env).reduce(
+      (accumulator: any, key: string) => {
+        if (key.startsWith('REACT_APP_')) {
+          accumulator[key] = process.env[key];
+        }
+        return accumulator;
+      },
+      {}
+    )
+  },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          sentry: ['@sentry/react', '@sentry/tracing']
+        }
+      }
+    }
+  },
+  resolve: {
+    conditions: ['development']
+  }
+});
