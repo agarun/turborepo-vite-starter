4bd84992e415804d5701b3a8344d71d4ea21fbf1
Aaron Agarunov <agarunov.aaron@gmail.com>
2022-12-22 12:40:36 -0500

    Use swc

--- apps/docs/vite.config.js
+++ apps/docs/vite.config.js
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
