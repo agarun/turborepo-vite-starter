373e6c4202a1bb00b9d0f1eaa09ea35ce2160dac
Aaron Agarunov <agarunov.aaron@gmail.com>
2022-12-23 13:15:34 -0500

    Add library generator

---
+++ tools/generators/library/src/lib/addLeadingZeros.js
@@ -0,0 +1,8 @@
+export default function addLeadingZeros(number, targetLength) {
+  const sign = number < 0 ? '-' : '';
+  let output = Math.abs(number).toString();
+  while (output.length < targetLength) {
+    output = `0${output}`;
+  }
+  return sign + output;
+}
